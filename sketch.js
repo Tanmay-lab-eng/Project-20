c10ba004a256c51cfbaf43f52b8173e7516463c1
Tanmay-lab-eng <72508346+Tanmay-lab-eng@users.noreply.github.com>
2021-04-19 19:53:36 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -4,8 +4,7 @@ var speed, weight;
 
 function setup() {
   createCanvas(1600,400);
-  createSprite(400, 200, 50, 50);
-
+  
   speed = random(55,90);
   weight = random(400,1500);
 
@@ -15,34 +14,26 @@ function setup() {
   wall.shapeColor=color(80,80,80);
   
   car.velocityX=speed;
-
- if (wall.x-car.x < (car.width+wall.width)/2){
-  car.velocityX=0;
-  var deformation = 0.5 * weight * speed * speed/22500;
-  if (deformation>180){
-    car.shapeColor=color(255,0,0);
-  }
-  if (deformation<180 && deformation>100){
-    car.shapeColor=color(230,230,0);
-  }
-  if (deformation<100){
-    car.shapeColor=color(0,255,0)
-  }
-}
+  car.shapeColor=color(255);
+  
+ 
 }
 
 
 function draw() {
-  background(255,255,255);  
+  background(255,255,255); 
+  if (wall.x-car.x < (car.width+wall.width)/2){
+    car.velocityX=0;
+    var deformation = 0.5 * weight * speed * speed/22500;
+    if (deformation>180){
+      car.shapeColor=color(255,0,0);
+    }
+    if (deformation<180 && deformation>100){
+      car.shapeColor=color(230,230,0);
+    }
+    if (deformation<100){
+      car.shapeColor=color(0,255,0)
+    }
+  } 
   drawSprites();
-}
-
-function detectCollision(wall,car){
-  carPosition = car.position
-  wallPosition = wall.position
-  
-  var distance = dist(carPosition.x,carPosition.y,wallPosition.x,wallPosition.y)
-  if (distance<=carPosition.x + wallPosition.x){
-    carPositon.x=0;
-  }
 }
